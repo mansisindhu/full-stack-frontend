c61e49208a9499ff5e2219c9ee6e472b204e5221
mansi sindhu <mansi.sindhu@cars24.com>
2024-11-29 19:54:06 +0530

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-function function1(param1) {
-  return function function2(param2) {
-    return param1 + param2
-  }
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import CreateBlog from './components/CreateBlog';
+import Blogs from './components/Blogs';
+import PrivateRoute from './components/PrivateRoute';
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      {/* Routes */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/create-blog' element={
+          <PrivateRoute>
+            <CreateBlog />
+          </PrivateRoute>} />
+        <Route path='/blogs' element={<Blogs />} />
+
+      </Routes>
+    </div>
+  );
 }
 
-const function1Return = function1(2);
-console.log(function1Return)
-const function2Return = function1Return(1)
-
-console.log(function2Return)
+export default App;
